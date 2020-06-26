01ae0e21752b2c01bd6ee5c1fb00985a735b3c58
Victor Yan <vyan.git@gmail.com>
2020-06-26 15:45:50 -0700

    Added react component generator script and reorganized folders

--- floatdeo-electronJS-app/webpack.config.js
+++ floatdeo-electronJS-app/webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'index.bundle.js',
